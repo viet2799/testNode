050b646a044495ecda0e02a8b309326e94587eb5
Nguyễn Đức Việt <43706629+viet2799@users.noreply.github.com>
2024-09-24 13:52:36 +0700

    .

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import usersRoutes from './routes/users.routes'
-import run from './services/database.services'
+import dataabaseService from './services/database.services'
 const app = express()
 
 app.use(express.json())
 app.use('/api/users', usersRoutes)
+require('dotenv').config()
+dataabaseService.connect()
 
-run().catch(console.dir)
 console.log('process.env.MONGO_URI', process.env.MONGO_URI)
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,14 +1,27 @@
-const { MongoClient, ServerApiVersion } = require('mongodb')
+import 'dotenv/config'
+import { Db, MongoClient } from 'mongodb'
 
-const URL = 'mongodb+srv://nguyenducviet2799:viet2799@tweets.1z6ba.mongodb.net/'
-const client = new MongoClient(URL as string)
-console.log('client', client)
-export default async function run() {
-  try {
-    await client.connect()
-    await client.db('admin').command({ ping: 1 })
-    console.log('Pinged your deployment. You successfully connected to MongoDB!')
-  } finally {
-    await client.close()
+const URL = process.env.REACT_APP_MONGO_URI
+const DB_NAME = process.env.REACT_APP_DB_NAME
+
+class DatabaseService {
+  private client: MongoClient
+  private dbName: Db
+  constructor() {
+    this.client = new MongoClient(URL as string)
+    this.dbName = this.client.db(DB_NAME)
+  }
+  async connect() {
+    try {
+      await this.dbName.command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    } finally {
+      await this.client.close()
+    }
   }
 }
+
+// taoj object tuwf class dataabaseService
+
+const dataabaseService = new DatabaseService()
+export default dataabaseService
